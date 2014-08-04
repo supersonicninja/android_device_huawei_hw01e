e6706c8a862a2bcc7add9c950b7ad0c73ff34a16
supersonicninja <niconicotor@live.com>
2014-08-04 21:26:59 +0900

    device:inherit msm8960.mk

--- device_hw01e.mk
+++ device_hw01e.mk
@@ -22,3 +22,5 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_hw01e
 PRODUCT_DEVICE := hw01e
+
+$(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
