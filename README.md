0e0865473c073b3c54153da5b3def819f82e552b
supersonicninjaxda <supersonicninajxda@live.com>
2014-12-18 04:38:35 -0800

    add files generated by mkvendor.sh from stock boot.img

--- README.md
+++
@@ -1,2 +0,0 @@
-android_device_huawei_hw01e
-===========================
--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := hw01e
+PRODUCT_RELEASE_NAME := u9501l
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/huawei/hw01e/device_hw01e.mk)
+$(call inherit-product, device/huawei/u9501l/device_u9501l.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := hw01e
-PRODUCT_NAME := cm_hw01e
+PRODUCT_DEVICE := u9501l
+PRODUCT_NAME := cm_u9501l
 PRODUCT_BRAND := huawei
-PRODUCT_MODEL := hw01e
+PRODUCT_MODEL := u9501l
 PRODUCT_MANUFACTURER := huawei
