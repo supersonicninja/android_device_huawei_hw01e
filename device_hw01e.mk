1c1ed894876ca04297a2ccc7c4c96c546375e78c
supersonicninja <supersonicninja@live.com>
2015-02-05 22:54:24 +0900

    device:make dalvik precise

--- device_hw01e.mk
+++ device_hw01e.mk
@@ -67,4 +67,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_hw01e
 PRODUCT_DEVICE := hw01e
 
+# Dalvik config
+PRODUCT_TAGS += dalvik.gc.type-precise
 $(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
